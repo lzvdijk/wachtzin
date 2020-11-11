f6fd7ec9fd4f70b1cd75cd5020fcf59e8ca3208a
Laurens van Dijk <laurens.vandijk@kpn-cert.nl>
2020-11-11 13:26:55 +0100

    trim wordlist

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Pseudorandom passphrase generator in Dutch.
 
 Wordlist originally created by: https://github.com/OpenTaal/opentaal-wordlist 
 
-Curated down to 158787 words by removing overly long words, words containing 
+Curated down to 144336 words by removing overly long words, words containing 
 special characters and potentially offensive phrases (don't judge I need to send these to customers)
 """
 
